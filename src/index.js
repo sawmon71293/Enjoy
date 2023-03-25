980210e0887ac6c8f8fc9d417c20a2918b97bb3a
sawmon <sawmonhan@acedatasystems.com>
2023-03-25 07:48:23 +0100

    refactored comment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import counter from "./modules/helper.js";
-import { shows } from "./modules/shows.js";
-import { populateView } from "./modules/render.js";
+import counter from './modules/helper.js';
+import { shows } from './modules/shows.js';
+import { populateView } from './modules/render.js';
 import './index.css';
 import { popup } from './modules/popup.js';
 
-
-
 const showCount = document.getElementById('show-count');
 const movieSection = document.getElementById('movies');
-
 
 const refresh = async () => {
   const data = await shows();
@@ -19,20 +16,13 @@ const refresh = async () => {
 
 document.addEventListener('DOMContentLoaded', async () => {
   await refresh();
-  const buttons = document.querySelectorAll('[data-target]');
-  buttons.forEach((link) => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
-      popup();
-    });
+  const movies = document.querySelector('#movies');
+
+  movies.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.matches('.comments-btn')) {
+      const movieId = event.target.dataset.id;
+      popup(movieId);
+    }
   });
 });
-
-
-
-
-
-
-
-
-
